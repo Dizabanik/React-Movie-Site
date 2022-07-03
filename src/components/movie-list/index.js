4dcec705a42215738fdb146365cd3dc7313ff78d
Dizab67 <dizabanik@gmail.com>
2022-07-03 19:40:14 +0200

    Added full beta pr

---
+++ src/components/movie-list/index.js
@@ -0,0 +1,19 @@
+import MovieCard from "../movie-card";
+
+export function MovieList({movies, selectMovie, onLoadMore}) {
+	const renderMovies = () => 
+		movies.map(movie => 	
+			<MovieCard
+				key={movie.id}
+				movie={movie}
+				selectMovie={selectMovie}
+			/>
+	);
+
+	return (
+		<div className='container'>
+			{renderMovies()}
+			<button className="btn-load-more" onClick={onLoadMore}>Load More</button>
+		</div>
+	)
+}
